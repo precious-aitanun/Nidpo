08397765d57f276546a1aa1f03f648871380916e
Nadira3 <paitanun35@gmail.com>
2025-10-12 10:18:09 -0700

    change root folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Nidpo/' // IMPORTANT: Replace 'your-repo-name' with your GitHub repository name
+  base: '/' // IMPORTANT: Replace 'your-repo-name' with your GitHub repository name
 })
